e17f97df1cb1276ef32c966109e6fceba7d49894
divya <divya1112singh1112@gmail.com>
2023-02-02 12:32:28 +0530

    list_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoppe/views/messageListScreen/list_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: logo()
+      home: msglist(),
     );
   }
 }
